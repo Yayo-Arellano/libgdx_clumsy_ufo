bdc09b62bf962c31d6d07ccd9bf389f087026520
Yayo <geramail28@gmail.com>
2021-03-17 10:56:40 +0800

    Compilable

--- android/src/com/nopalsoft/clumsy/AndroidLauncher.java
+++ android/src/com/nopalsoft/clumsy/AndroidLauncher.java
@@ -5,12 +5,99 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.nopalsoft.clumsy.MainClumsy;
+import com.nopalsoft.clumsy.handlers.GameServicesHandler;
+import com.nopalsoft.clumsy.handlers.RequestHandler;
 
-public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MainClumsy(), config);
-	}
+public class AndroidLauncher extends AndroidApplication implements RequestHandler, GameServicesHandler {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new MainClumsy(this, this), config);
+    }
+
+    @Override
+    public void submitScore(String leaderboard, long score) {
+
+    }
+
+    @Override
+    public void unlockAchievement(String achievementId) {
+
+    }
+
+    @Override
+    public void getLeaderboard() {
+
+    }
+
+    @Override
+    public void getAchievements() {
+
+    }
+
+    @Override
+    public boolean isSignedIn() {
+        return false;
+    }
+
+    @Override
+    public void signIn() {
+
+    }
+
+    @Override
+    public void signOut() {
+
+    }
+
+    @Override
+    public void showRater() {
+
+    }
+
+    @Override
+    public void showInterstitial() {
+
+    }
+
+    @Override
+    public void showFacebook() {
+
+    }
+
+    @Override
+    public void showMoreGames() {
+
+    }
+
+    @Override
+    public void shareOnFacebook(String mensaje) {
+
+    }
+
+    @Override
+    public void shareOnTwitter(String mensaje) {
+
+    }
+
+    @Override
+    public void showAdBanner() {
+
+    }
+
+    @Override
+    public void hideAdBanner() {
+
+    }
+
+    @Override
+    public void comprarRemoveAds() {
+
+    }
+
+    @Override
+    public void restorePurchases() {
+
+    }
 }
